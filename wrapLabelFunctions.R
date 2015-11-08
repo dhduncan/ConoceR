685e5a60a7a636a8e3eec6471f000e3eb5862e75
dhduncan <davidduncan@AirPuppy4.local>
2015-11-07 21:44:53 -0500

    Nuevos temas

---
+++ wrapLabelFunctions.R
@@ -0,0 +1,18 @@
+# Core wrapping function
+wrap.it <- function(x, len)
+{ 
+  sapply(x, function(y) paste(strwrap(y, len), 
+                              collapse = "\n"), 
+         USE.NAMES = FALSE)
+}
+
+# Call this function with a list or vector
+wrap.labels <- function(x, len)
+{
+  if (is.list(x))
+  {
+    lapply(x, wrap.it, len)
+  } else {
+    wrap.it(x, len)
+  }
+}
