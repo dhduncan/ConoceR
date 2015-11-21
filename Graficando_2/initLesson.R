f8a01a24557441c50bb73325c72ff9bcecc3c255
dhduncan <dhduncan1975@gmail.com>
2015-11-21 15:52:22 -0500

    finalizar contenido de la lección

--- Graficando_2/initLesson.R
+++ Graficando_2/initLesson.R
@@ -2,3 +2,23 @@
 # lesson is started. Any variables created here will show up in
 # the user's working directory and thus be accessible to them
 # throughout the lesson.
+
+.pathtofile <<- function(course_, lesson_, file_){
+  if(as.character(packageVersion("swirl")) > "2.2.21"){
+    file.path(get_swirl_option("courses_dir"), course_, lesson_, file_)
+  } else {
+    file.path(find.package("swirl"), "Courses", course_, lesson_, file_)
+  }
+}
+
+# Make path to xlsx available to user
+path2csv1 <- file.path(path.package('swirl'), 'Courses',
+                       'ConoceR',
+                       'Graficando_1',
+                       'hijosResumen.csv')
+
+# Create datasets for user. We don't advertise that we're
+# doing this, but it will be necessary for students who
+# quit and later resume. We are not saving the variable
+# to the progress file to save on performance.
+resumen <- read.csv(path2csv1)
