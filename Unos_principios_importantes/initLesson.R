76de082481abb4a43df5dc2747ef0b3fb1782ccc
seankross <smk240@gmail.com>
2016-04-06 16:32:44 -0400

    fixed course location issue

--- Unos_principios_importantes/initLesson.R
+++ Unos_principios_importantes/initLesson.R
@@ -1,7 +1,21 @@
 library(ggplot2)
 library(jpeg)
+
+# Code placed in this file fill be executed every time the
+# lesson is started. Any variables created here will show up in
+# the user's working directory and thus be accessible to them
+# throughout the lesson.
+
+.pathtolesson <<- function(course_, lesson_){
+	course_path <- tryCatch(swirl:::swirl_courses_dir(),
+           error = function(c) {file.path(find.package("swirl"),"Courses")}
+  )
+  file.path(course_path, course_, lesson_)
+}
+
+
 # Put initialization code in this file.
-path_to_course <- file.path(find.package("swirl"),"MOOC/ConoceR/Unos_principios_importantes")
+path_to_course <- .pathtolesson("ConoceR", "Unos_principios_importantes")
 plot.new()
 
 # Code placed in this file fill be executed every time the
